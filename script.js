0f26c9f018b92c4d31fd0dfd2ac110e6efdbf019
solomon123-web <chimeremezematthew48@gmail.com>
2026-02-01 16:55:21 -0800

    index.html

---
+++ script.js
@@ -0,0 +1,18 @@
+function myfunction() {
+    alert("Kingdom worship TV is not available for now.")
+}
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
